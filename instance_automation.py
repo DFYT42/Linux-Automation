0e1ac6b41b6480b77a96ae8828830d4cedbe7aca
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2018-11-15 20:07:25 -0800

    Create instance_automation.py
    automating instance

---
+++ instance_automation.py
@@ -0,0 +1,113 @@
+#! /usr/bin/python
+
+##https://cloud.google.com/compute/docs/tutorials/python-guide
+##centos7
+
+from oauth2client.client import GoogleCredentials
+from googleapiclient import discovery
+
+import pprint
+import json
+#import create_ldap
+#from cretae_ldap import create_instance
+
+credentials = GoogleCredentials.get_application_default()
+compute = discovery.build('compute','vi', credentials=credentials)
+
+project = "artful-sled-217801"
+zone = "us-east1-b"
+
+#machine type requested and name
+#machine type helps derive name
+
+name = lower.(input("what is machine name? "))
+
+def list_instances(compute, project, zone):
+    result = compute.instances().list(project=project, zone=zone).execute()
+    return result['items']
+  
+def create_instance(compute, project, zone, name, bucket):
+    # Configure the machine
+    startup_script = open('startup-script.sh', 'r').read()
+    image_response = compute.image().getFromFamily(project='centos-cloud', family='centos-7').execute()
+
+    config = {
+        'name': name,
+        'machineType': machine_type,
+        
+        # Specify the boot disk and the image to use as a source.
+        'disks': [
+            {
+                'boot': True,
+                'autoDelete': True,
+                'initializeParams': {
+                    'sourceImage': source_disk_image,
+                }
+            }
+        ],
+
+        # Specify a network interface with NAT to access the public
+        # internet.
+        'networkInterfaces': [{
+            'network': 'global/networks/default',
+            'accessConfigs': [
+                {'type': 'ONE_TO_ONE_NAT', 'name': 'External NAT'}
+            ]
+        }],
+
+        # Allow the instance to access cloud storage and logging.
+        'serviceAccounts': [{
+            'email': 'default',
+            'scopes': [
+                'https://www.googleapis.com/auth/devstorage.read_write',
+                'https://www.googleapis.com/auth/logging.write'
+            ]
+        }],
+
+        #Enable https/http for select instances
+        "labels": {
+        "http-server": "",
+        "https-server": "",
+        },
+        
+        "tags": {
+        "items": [
+        "http-server",
+        "https-server"
+        ]
+        },
+        
+        # Metadata is readable from the instance and allows you to
+        # pass configuration from deployment scripts to instances.
+        'metadata': {
+            'items': [{
+                # Startup script is automatically executed by the
+                # instance upon startup.
+                'key': 'startup-script',
+                'value': startup_script
+            }, {
+                'key': 'url',
+                'value': image_url
+            }, {
+                'key': 'text',
+                'value': image_caption
+            }, {
+                'key': 'bucket',
+                'value': bucket
+            }]
+        }
+    }
+
+    return compute.instances().insert(
+        project=project,
+        zone=zone,
+        body=config).execute()
+
+newinstance = create_instance(compute,project, zone, name)
+instance = list_instances(compute, project, zone)
+
+pprint.pprint(newinstance)
+pprint.pprint(instance)
+
+
+
