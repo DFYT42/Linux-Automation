0bf7b7382adb093a8c66481207c6b2eef7489cab
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2018-11-20 14:11:01 -0800

    Update instance_automation.py
    uncommeneted discovery

--- instance_automation.py
+++ instance_automation.py
@@ -4,7 +4,7 @@
 ##centos7
 
 from oauth2client.client import GoogleCredentials
-#from googleapiclient import discovery
+from googleapiclient import discovery
 
 import googleapiclient
 import pprint
