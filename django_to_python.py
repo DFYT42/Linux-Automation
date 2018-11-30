c9964a62dbb4bc4bd4850455d0d891bb3dcd99cd
root <root@admin-1.us-east1-b.c.artful-sled-217801.internal>
2018-11-30 05:02:08 +0000

    commented out redundant lines and now works--still get yellow hosts not allowed screen but that means need to uncomment the push to allowed host lines

--- django_to_python.py
+++ django_to_python.py
@@ -13,10 +13,10 @@ os.chdir('/opt/django/')
 ##running python 2 but want 3.6
 os.system('yum -y install python36')
 os.system('virtualenv -p python36 django')  
-os.system('source /opt/django/django/bin/activate && pip install django')
+#os.system('source /opt/django/django/bin/activate && pip install django')
     
 ##still in 2.7.5 && need to source to 3.6
-os.chdir('/opt/django')
+#os.chdir('/opt/django')
 os.system('source /opt/django/django/bin/activate && pip install django' + \
           '&& django-admin --version ' + \
           '&& django-admin startproject project1')
@@ -25,7 +25,7 @@ os.system('source /opt/django/django/bin/activate && pip install django' + \
     
 ##change ownership from root to a user
 os.system('chown -R g42dfyt /opt/django')  ##if you don't know this--: who
-os.system('chown -R g42dfyt /opt/django/project1')
+#os.system('chown -R g42dfyt /opt/django/project1')
 #os.system("myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//') && sed -i \"s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g\" /opt/django/project1/project1/settings.py")
 os.system('sudo -u g42dfyt  sh -c "source /opt/django/django/bin/activate && python /opt/django/project1/manage.py runserver 0.0.0.0:8000&" ')
 
