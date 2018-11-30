cbdcd0279cf543bed4e15333180b077dcf493349
root <root@admin-1.us-east1-b.c.artful-sled-217801.internal>
2018-11-30 04:43:56 +0000

    added pip install django and checked against Amrit

--- django_to_python.py
+++ django_to_python.py
@@ -17,7 +17,7 @@ os.system('source /opt/django/django/bin/activate && pip install django')
     
 ##still in 2.7.5 && need to source to 3.6
 os.chdir('/opt/django')
-os.system('source /opt/django/django/bin/activate ' + \
+os.system('source /opt/django/django/bin/activate && pip install django' + \
           '&& django-admin --version ' + \
           '&& django-admin startproject project1')
 
@@ -26,7 +26,7 @@ os.system('source /opt/django/django/bin/activate ' + \
 ##change ownership from root to a user
 os.system('chown -R g42dfyt /opt/django')  ##if you don't know this--: who
 os.system('chown -R g42dfyt /opt/django/project1')
-os.system("myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//') && sed -i \"s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g\" /opt/django/project1/project1/settings.py")
+#os.system("myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//') && sed -i \"s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g\" /opt/django/project1/project1/settings.py")
 os.system('sudo -u g42dfyt  sh -c "source /opt/django/django/bin/activate && python /opt/django/project1/manage.py runserver 0.0.0.0:8000&" ')
 
 ##activate django install and run server
