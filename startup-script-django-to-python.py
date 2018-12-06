104353596d4385a9b23946e3eeb6041250466112
Doug Lasa <27441098+DFYT42@users.noreply.github.com>
2018-12-05 18:05:43 -0800

    Update startup-script-django-to-python.py
    neatened up

--- startup-script-django-to-python.py
+++ startup-script-django-to-python.py
@@ -12,20 +12,17 @@ os.chdir('/opt/django/')
     
 ##running python 2 but want 3.6
 os.system('yum -y install python36')
-os.system('virtualenv -p python36 django')  
-#os.system('source /opt/django/django/bin/activate && pip install django')
+os.system('virtualenv -p python36 django')
     
-##still in 2.7.5 && need to source to 3.6
-#os.chdir('/opt/django')
+##start project
 os.system('source /opt/django/django/bin/activate && pip install django' + \
           '&& django-admin --version ' + \
           '&& django-admin startproject project1')
-
-##start project
     
 ##change ownership from root to a user
 os.system('chown -R g42dfyt /opt/django')  ##if you don't know this--: who
 #os.system('chown -R g42dfyt /opt/django/project1')
+#seek current ip and place in ALLOWED HOSTS
 os.system("myip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//') && sed -i \"s/ALLOWED_HOSTS = \[\]/ALLOWED_HOSTS = \[\'$myip\'\]/g\" /opt/django/project1/project1/settings.py")
 os.system('sudo -u g42dfyt  sh -c "source /opt/django/django/bin/activate && python /opt/django/project1/manage.py runserver 0.0.0.0:8000&" ')
 
